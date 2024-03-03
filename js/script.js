165c79af1216d6a79c3f174ea386ac3216fdb03d
Sean Ahn <edotakeout@gmail.com>
2024-03-02 21:44:53 -0500

    set up for local storage

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var searchInput = $('#search-input');
 var searchBtn = $('#search-btn');
 
 var apiKey = '28afe2d5b1bd9dc13c52bb3af017e6f9'
-var currentURL = `https://api.openweathermap.org/data/2.5/weather?q=atlanta&appid=${apiKey}&units=imperial`;
+var currentURL = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=imperial`;
 var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=atlanta&appid=${apiKey}&units=imperial`;
 
 // $.get(forecastURL)
@@ -26,11 +26,38 @@ function getSearchHistory() {
     return history;
 }
 
+function getForecastWeather() {
+    console.log(forecast);
+}
 
 function getCurrentForecast() {
     var cityName = searchInput.val();
+    var history = getSearchHistory();
 
-    getSearchHistory();
+    if (!history.includes(cityName)) {
+        // Add city to empty array
+        history.push(cityName);
+        //Replace the old history array in localstorage with search
+        localStorage.setItem('search-history', JSON.stringify(history));
+    }
+
+    //Make a request for current weather using the url and insert the city name value at the end
+    $.get(currentURL + `&q=${cityName}`)
+        .then(function (data) {
+        // Output the current Weather conditions
+        
+
+
+        // Retrieve the forecast weather
+   
+        })
+        .then(getForecastWeather)
 }
 
 searchBtn.click(getCurrentForecast);
+
+
+// //example of event delegation at parent level.  all button will output 'clicked'
+// $('.history').on('click', 'button', function() {
+//     console.log('clicked');
+// })
